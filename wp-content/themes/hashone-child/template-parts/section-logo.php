2d514dccb7f48c2e103d8b78e635b8499714e6a2
Max <lutsenko.maxim@gmail.com>
2017-03-08 18:54:03 +0200

    Clients logo section

--- wp-content/themes/hashone-child/template-parts/section-logo.php
+++ wp-content/themes/hashone-child/template-parts/section-logo.php
@@ -19,38 +19,37 @@ if( get_theme_mod('hashone_disable_logo_sec') != 'on' ){ ?>
 			<?php if($hashone_logo_sub_title){ ?>
 				<div class="hs-section-tagline wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.5s"><?php echo esc_html($hashone_logo_sub_title); ?></div>
 			<?php } ?>
-			<?php
-			for( $i = 1; $i < 11 ; $i++ ) {
-				?>
-				<?php
-				$hashone_client_logo_image = get_theme_mod('hashone_client_logo_image'.$i);
-//				$hashone_client_logo_image = explode(',', $hashone_client_logo_image);
-				$hashone_client_logo_url = get_theme_mod('hashone_client_logo_url'.$i);
-//				$hashone_client_logo_url = explode(',', $hashone_client_logo_url);
 
-//				var_dump($hashone_client_logo_image, $hashone_client_logo_url );
-//				exit;
-
-				if($hashone_client_logo_image){
-					$args = array( 'page_id' => $hashone_client_logo_image );
-					$query = new WP_Query($args);
-					if($query->have_posts()):
-						while($query->have_posts()) : $query->the_post();
-							?>
-							<div class="wow zoomIn" data-wow-duration="0.5s" data-wow-delay="0.5s">
-								<div class="hs_client_logo_slider">
-									<a target="_blank" href="<?php echo esc_url($hashone_client_logo_url) ?>">
-										<img alt="<?php _e('logo','hashone') ?>" src="<?php echo $hashone_client_logo_image; ?>">
-									</a>
-								</div>
-							</div>
-							<?php
-						endwhile;
-					endif;
-					wp_reset_postdata();
-				}
-			}
-			?>
+			<div class="wow zoomIn" data-wow-duration="0.5s" data-wow-delay="0.5s">
+				<div class="hs_client_logo_slider">
+					<?php
+					for( $i = 1; $i < 11 ; $i++ ) {
+						?>
+						<?php
+						$hashone_client_logo_image = get_theme_mod('hashone_client_logo_image'.$i);
+		//				$hashone_client_logo_image = explode(',', $hashone_client_logo_image);
+						$hashone_client_logo_url = get_theme_mod('hashone_client_logo_url'.$i);
+		//				$hashone_client_logo_url = explode(',', $hashone_client_logo_url);
+		//				var_dump($hashone_client_logo_image, $hashone_client_logo_url );
+		//				exit;
+						if($hashone_client_logo_image){
+							$args = array( 'page_id' => $hashone_client_logo_image );
+							$query = new WP_Query($args);
+							if($query->have_posts()):
+								while($query->have_posts()) : $query->the_post();
+									?>
+											<a target="_blank" href="<?php echo esc_url($hashone_client_logo_url) ?>">
+												<img alt="<?php _e('logo','hashone') ?>" src="<?php echo $hashone_client_logo_image; ?>">
+											</a>
+									<?php
+								endwhile;
+							endif;
+							wp_reset_postdata();
+						}
+					}
+					?>
+				</div>
+			</div>
 		</div>
 	</section>
 <?php } ?>
